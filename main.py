e4f0b4a1fd2c7b80677b98fe376edf3da2fe6a54
Jeremy Rode <jprode@gmail.com>
2025-08-27 07:31:43 -0700

    org changes

--- main.py
+++ main.py
@@ -8,24 +8,26 @@ import ujson
 
 print('boot')
 ## GPIO Setup for pump buttons
-power_button = machine.Pin(9, mode=machine.Pin.OUT, pull=None, value=0)
-pause_button = machine.Pin(11, mode=machine.Pin.OUT, pull=None, value=0)
-up_button = machine.Pin(14, mode=machine.Pin.OUT, pull=None, value=0)
-down_button = machine.Pin(15, mode=machine.Pin.OUT, pull=None, value=0)
+power_button = machine.Pin(9, mode=machine.Pin.OUT, pull=None, value=0) # type: ignore
+pause_button = machine.Pin(11, mode=machine.Pin.OUT, pull=None, value=0) # type: ignore
+up_button = machine.Pin(14, mode=machine.Pin.OUT, pull=None, value=0) # type: ignore
+down_button = machine.Pin(15, mode=machine.Pin.OUT, pull=None, value=0) # type: ignore
 ## ADC Setup
-ct1_sensor_adc = machine.ADC(machine.Pin(26))     # 
-ct2_sensor_adc = machine.ADC(machine.Pin(27))     #
-level_sensor_adc = machine.ADC(machine.Pin(28))   # 
+ct1_sensor_adc = machine.ADC(machine.Pin(26))    
+ct2_sensor_adc = machine.ADC(machine.Pin(27))   
+level_sensor_adc = machine.ADC(machine.Pin(28))  
 temp_sensor_adc = machine.ADC(4)
 # Timers
-button_press_timer = machine.Timer()
-logging_timer = machine.Timer()
-log_retry_timer = machine.Timer()
+feedback_loop_timer = machine.Timer() # type: ignore
+logging_timer = machine.Timer() # type: ignore
+log_retry_timer = machine.Timer() # type: ignore
 # WLAN Setup
 wlan = network.WLAN(network.STA_IF)
 # Globals
 DATA_IIR_CONST = 1000  # Filtering constant for the IIR filters
-depth = 0 #
+depth = 0 #Current measured IIR depth
+depth_max = 10
+depth_min = 5
 
 def all_buttons_off():
     power_button.value(0)
@@ -34,12 +36,12 @@ def all_buttons_off():
     down_button.value(0)
 
 def button_delay(): #Call this after a button press
-    button_press_timer.init(mode=machine.Timer.ONE_SHOT,period=600_000,callback=all_buttons_off) # 
+    button_press_timer.init(mode=machine.Timer.ONE_SHOT,period=600_000,callback=all_buttons_off) # type: ignore 
     
 # Logging Function via Wifi
 def logDataWifi():
     data = {"key1": "value1", "key2": 123} # Take the data
-    if not wlan.isconnected() and wifilog:
+    if not wlan.isconnected():
         print(f"Reconnecting to WiFi Network Name: {secrets.ssid}")
         try:
             wlan.active(True)
@@ -65,9 +67,18 @@ def logDataWifi():
                 print('Unexpected Post Response:', res.text)
 
 
+def pumpSpeedFeedback():
+    if depth > depth_max:  # If depth greater than upper target
+        up_button.value(1) # Speed the pump up
+        button_delay()
+    elif depth < depth_min: # If depth greater than lower target (Will this go into pause mode??)
+        down_button.value(1) # Slow the pump down
+        button_delay()
+
 # Start up the timers
 logging_timer.init(mode=machine.Timer.PERIODIC,period=900_000,callback=logDataWifi)
+feedback_loop_timer.init(mode=machine.Timer.PERIODIC,period=10_000,callback=pumpSpeedFeedback)
 
 while True: #Main loop, just take measurments
-    baseline = curval / DATA_IIR_CONST + baseline * (DATA_IIR_CONST - 1) / DATA_IIR_CONST #Take Baseline Stats
+    depth = level_sensor_adc.read_u16() / DATA_IIR_CONST + depth * (DATA_IIR_CONST - 1) / DATA_IIR_CONST 
 
